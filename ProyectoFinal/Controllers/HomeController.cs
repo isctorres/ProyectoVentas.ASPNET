94355181aacf969373dc997906a3dddf331beda5
isctorres <isctorres@gmail.com>
2019-03-30 11:13:38 -0600

    trabajo hecho en clase

--- ProyectoFinal/Controllers/HomeController.cs
+++ ProyectoFinal/Controllers/HomeController.cs
@@ -4,15 +4,28 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProyectoFinal.Data;
 using ProyectoFinal.Models;
 
 namespace ProyectoFinal.Controllers
 {
     public class HomeController : Controller
     {
+        // Agregamos el contexto al controlador para poder hacer consultas
+        private readonly ApplicationDbContext _context;
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var promos = (from n in _context.Promociones.Include("Productos")
+                            //where n.FechaInicio >= DateTime.Now && DateTime.Now <= n.FechaFin
+                            select n);
+
+            return View(promos.ToList());
         }
 
         public IActionResult Privacy()
